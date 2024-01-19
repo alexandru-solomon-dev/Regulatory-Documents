94558c3dc1b711c2f7c9255124bb187d5b232c20
Krasnov-Midnight <118168225+Krasnov-Midnight@users.noreply.github.com>
2024-01-19 13:04:16 +0200

    add example for white space rules

--- Example/AttributeExample.cs
+++ Example/AttributeExample.cs
@@ -4,46 +4,49 @@ using Random = UnityEngine.Random;
 
 namespace Example
 {
-   /// <summary>
-   /// Example of attribute usage rules.
-   /// </summary>
-   [ExecuteInEditMode]
-   [CreateAssetMenu(menuName = "Example/AttributeUsage", fileName = "AttributeExample")]
-   public class AttributeExample : ScriptableObject
-   {
-      private const int _minimum = 0;
-      private const int _maximum = 0;
+  /// <summary>
+  /// Example of attribute usage rules.
+  /// </summary>
+  [ExecuteInEditMode]
+  [CreateAssetMenu(menuName = "Example/AttributeUsage", fileName = "AttributeExample")]
+  public class AttributeExample : ScriptableObject
+  {
+    private const int _minimum = 0;
+    private const int _maximum = 0;
 
-      [HideInInspector] public int DontVisibleForInspector;
-      
-      [Header("Functional variables block")]
-      [Tooltip("Description for field")]
-      [Range(_minimum, _maximum)]
-      [SerializeField]
-      private float _powerValue = _minimum;
-      [SerializeField] 
-      private int _multiplier1 = 0;
-      [Tooltip("Description for field")]
-      [Min(0)]
-      [SerializeField]
-      private int _multiplier2;
-      
-      [Header("Functional variables block 2")]
-      [SerializeField] private int _functionalVarriable1;
-      [SerializeField] private int _functionalVarriable2;
-      [SerializeField] private int _functionalVarriable3;
-      [SerializeField] private int _functionalVarriable4;
+    [HideInInspector] public int DontVisibleForInspector;
 
-      [ContextMenu("Change Functional Value")]
-      private void SetRandomValue()
-      {
-         _powerValue = Random.Range(_minimum, _maximum);
-      }
-      
-      [ContextMenu("Print Result")]
-      private void PrintResult()
-      {
-         Debug.Log(Math.Pow(_multiplier1 * Math.Abs(_multiplier2), _powerValue));
-      }
-   }
+    [Header("Functional variables block")]
+    [Tooltip("Description for field")]
+    [Range(_minimum, _maximum)]
+    [SerializeField]
+    private float _powerValue = _minimum;
+
+    [SerializeField] private int _multiplier1 = 0;
+
+    [Tooltip("Description for field")]
+    [Min(0)]
+    [SerializeField]
+    private int _multiplier2;
+
+    [Header("Functional variables block 2")]
+    [SerializeField]
+    private int _functionalVarriable1;
+
+    [SerializeField] private int _functionalVarriable2;
+    [SerializeField] private int _functionalVarriable3;
+    [SerializeField] private int _functionalVarriable4;
+
+    [ContextMenu("Change Functional Value")]
+    private void SetRandomValue()
+    {
+      _powerValue = Random.Range(_minimum, _maximum);
+    }
+
+    [ContextMenu("Print Result")]
+    private void PrintResult()
+    {
+      Debug.Log(Math.Pow(_multiplier1 * Math.Abs(_multiplier2), _powerValue));
+    }
+  }
 }
---
+++ Example/WhiteSpacesRules.cs
@@ -0,0 +1,137 @@
+using System; 
+
+namespace Example
+{
+  //Can be written in a single line, but the recommended form is as in the example below for ShortEnumWithAssignment
+  public enum ShortEnumWithoutAssignment { None, Something }
+  
+  //Cannot be written in one line, because assignment of the value
+  public enum ShortEnumWithAssignment
+  {
+    First = 0,
+    Second = 1,
+    Third = 2
+  }
+
+  /// <summary>
+  /// Example white spaces rules.
+  /// </summary>
+  public class WhiteSpacesRules
+  {
+    private class Results
+    {
+      public int CountNegativeResults = 0;
+      public int CountPositiveResults = 0;
+    }
+
+    private const int _lineMultiplier = 1907;
+    
+    private readonly Results _results;
+
+    // Writing a short array in one line
+    private readonly int[] _someShortArray = { 1, 2, 3 };
+    
+    // Container initializers use a 2
+    private readonly int[,] _someTwoDimensionalArray =
+    {
+      {1, 2, 3},
+      {4, 5, 6},
+      {7, 8, 9}
+    };
+
+    public WhiteSpacesRules()
+    {
+      // Object initializers use a 2 space indent.
+      _results = new Results
+      {
+        CountNegativeResults = 0,
+        CountPositiveResults = 0
+      };
+    }
+
+    public int ConditionsAndCycles(int mulNumber)
+    {
+      int lineNumber = 0;
+      int columnNumber = 0;
+      int columnCounts = _someTwoDimensionalArray.GetLength(0);
+
+      foreach (int current in _someTwoDimensionalArray)
+      {
+        // Exception for using braces. 'if' with using 'return'/'continue'/'break'.
+        if (current == mulNumber) // Spaces around comparison operator.
+          return CalculateResult();
+        
+        columnNumber++;
+        // Braces used even when optional.
+        if (columnNumber >= columnCounts) // Spaces around comparison operator.
+        {
+          NewLine();
+        }
+      }
+
+      _results.CountNegativeResults++;
+      return -1;
+      
+      // Local functions.
+      void NewLine()
+      {
+        lineNumber++;
+        columnNumber = 0;
+      }
+      
+      int CalculateResult()
+      {
+        _results.CountPositiveResults++;
+        return _lineMultiplier * lineNumber + columnNumber;
+      }
+    }
+
+    public void ExpressionBodies()
+    {
+      // For simple lambdas, fit on one line if possible, no brackets or braces required.
+      Func<int, int> increment = x => x + 1;
+
+      // Closing brace aligns with first character on line that includes the opening brace.
+      Func<int, int, long> difference1 = (x, y) =>
+      {
+        long diff = (long)x - y;
+        return diff >= 0 ? diff : -diff;
+      };
+
+      // If defining after a continuation line break, indent the whole body.
+      Func<int, int, long> difference2 =
+        (x, y) =>
+        {
+          long diff = (long)x - y;
+          return diff >= 0 ? diff : -diff;
+        };
+    }
+
+    // Empty blocks may be concise.
+    private void DoNothing() { } 
+
+    // If possible, wrap arguments by aligning newlines with the first argument.
+    private void AVeryLongFunctionNameThatCausesLineWrappingProblems(int longArgumentName, int longArgumentName2, 
+                                                                     int longArgumentName3)
+    { }
+
+    // If aligning argument lines with the first argument doesn't fit, or is difficult to
+    // read, wrap all arguments on new lines with a 4 space indent.
+    private void AnotherVeryLongFunctionNameThatCausesLineWrappingProblems(
+        int longArgumentName, int longArgumentName2, int longArgumentName3, int longArgumentName4, int longArgumentName5)
+    { }
+
+    private void CallingLongFunctionName()
+    {
+      int veryLongArgumentName = 1234;
+      int shortArg = 1;
+      // If possible, wrap arguments by aligning newlines with the first argument.
+      AnotherVeryLongFunctionNameThatCausesLineWrappingProblems(shortArg, shortArg, veryLongArgumentName, 
+                                                                veryLongArgumentName, veryLongArgumentName);
+      // If aligning argument lines with the first argument doesn't fit, or is difficult to
+      // read, wrap all arguments on new lines with a 4 space indent.
+      AnotherVeryLongFunctionNameThatCausesLineWrappingProblems(
+          veryLongArgumentName, veryLongArgumentName, veryLongArgumentName, veryLongArgumentName,veryLongArgumentName);
+    }
+  }
+}
